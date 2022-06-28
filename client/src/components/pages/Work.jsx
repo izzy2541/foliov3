45c6bcb6d9fcc9dfe02a376f00a571f17c4814f6
izzy2541 <71002968+izzy2541@users.noreply.github.com>
2022-06-28 21:29:08 +0200

    adding in work

--- client/src/components/pages/Work.jsx
+++ client/src/components/pages/Work.jsx
@@ -5,6 +5,8 @@ import mybookshelf from '../../images/bookshelfmockupp.png'
 import irk from '../../images/irk3.png'
 import gh1 from '../../images/gh1.png'
 import podsite from '../../images/podsite.png'
+import zodiac from '../../images/myzodiac.jpg'
+import mysite from '../../images/studioisabelhome.jpg'
 
 
 
@@ -23,8 +25,10 @@ function Work() {
                         <h3>My Bookshelf</h3>
                             <p>I built the My Bookshelf app using React and styled it using Bootstrap and styled components. Here users can login, and create a virtual display of the books they have been reading.</p>
                                 <div className="card-buttons">
-                                    <Button variant="outline-dark">Source Code</Button>
-                                    <Button variant="outline-dark">Live Site</Button>
+                                    <a href="https://github.com/izzy2541/mybooks" target="_blank">
+                                    <Button className="btn" variant="outline-dark" >Source Code</Button>
+                                    </a>
+                                
                                 </div>
                 </div>
             </Col>
@@ -69,6 +73,40 @@ function Work() {
                             <Link to="/work/greenhouse">
                                 <Button variant="outline-dark"> View Project</Button>
                             </Link>
+                        </div> 
+                  </div>               
+                  </Col>
+                  <Col>
+                  <div className= "work-card">
+                    <img className="work-image" src={zodiac} variant="top" alt="my project"/>  
+                        <h3>Astrology.Com</h3>
+                         <p>A website where a user can find out what zodiac they are based on their date of birth and also find out more informatio about said zodiac sign. This was my first Next.js project! I also used SASS.
+
+                         </p>
+                  
+                        <div className="card-buttons">
+                            <a href="https://github.com/izzy2541/astrologyv4" target="_blank">
+                                <Button className="btn" variant="outline-dark" >Source Code</Button>
+                            </a>
+                            <a href="https://myzodiac.vercel.app/" target="_blank">
+                                <Button  className="btn"  variant="outline-dark"> Live Site</Button>  
+                            </a> 
+                        </div>  
+                </div> 
+                  </Col>
+                  <Col sm={12} md={6}>
+                  <div className="work-card" >
+                    <img className="work-image" variant="top" src={mysite} alt="my project"/>  
+                        <h3>Studio Isabel</h3>
+                         <p> I built my folio website from scratch using React, SASS and Bootstrap. Have a look around! I am really proud of this site.</p>    
+
+                        <div className="card-buttons">
+                        <a href="https://github.com/izzy2541/foliov3" target="_blank">
+                                <Button className="btn" variant="outline-dark" >Source Code</Button>
+                            </a>
+                            <a href="https://isabel-donnelly.com/" target="_blank">
+                                <Button  className="btn"  variant="outline-dark"> Live Site</Button>  
+                            </a> 
                         </div> 
                   </div>               
                   </Col>
