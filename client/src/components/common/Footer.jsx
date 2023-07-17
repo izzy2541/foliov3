48ef3c783046e05169bdfcd7bd90062e12f161e4
izzy2541 <71002968+izzy2541@users.noreply.github.com>
2023-07-17 19:06:27 +1000

    Update Footer.jsx

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="myfooter">
-           Studio Isabel &copy; 2022
+           Studio Isabel &copy; 2023
         </footer>
     );
 }
