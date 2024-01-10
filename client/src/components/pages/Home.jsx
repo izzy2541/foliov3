caba3d41ec04863876ff78e8419c5ab918e56c94
izzy2541 <71002968+izzy2541@users.noreply.github.com>
2024-01-10 19:45:48 +1100

    Update Home.jsx

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -8,8 +8,7 @@ function Home() {
     <Fragment>
       <div className="HomePage">
         <div className="studio-text">
-            STUDIO
-            ISABEL
+            ISABELS FOLIO
         </div>
         <div className="paragraph-text" colSpan="5"> Hi! My names Isabel and I'm a Front-End Web Developer<br></br> and Graphic Designer based in Melbourne Australia. <br></br>
       Welcome to my portfolio, please <Link to="work"><u>have a look</u></Link> and contact me for further enquires.
